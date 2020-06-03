8f7ee02e4f73767c585f691e90a8a5ff805bff15
Matthew <herrigesmt@gmail.com>
2020-06-03 15:13:25 -0700

    added correct .json for lint, created directories for css and js, added questions to app.js, added framework for lists in index.html and fixed js and css links

--- js/app.js
+++ js/app.js
@@ -42,18 +42,18 @@ Does Matt prefer Summer, Winter, Fall, or Spring?
 */
 
 var askNameAnswer = prompt('What is your name, visitor?');
-
-var pizzaAnswer = prompt('Okay, '+ askNameAnswer + '. Does Matt like Pineapple on pizza?');
-pizzaAnswer = pizzaAnswer.toLowerCase();
-
+var answersCorrect = 0;
+var pizzaAnswer = prompt('Okay, '+ askNameAnswer + '. Does Matt like Pineapple on pizza?').toLowerCase();
+//  question one ============================
 if (pizzaAnswer === 'yes' || pizzaAnswer === 'y' || pizzaAnswer === 'yeah') {
   alert('Ew, no, pineapples on pizza are gross.');
   // console.log('Ew, pineapples are gross');
 } else {
   alert('Correct, pineapples on pizza are gross');
   // console.log('Correct, pineapples on pizza are gross');
+  answersCorrect++;
 }
-
+//  question two =============================
 var petAnswer = prompt('Does Matt have any pets?');
 petAnswer = petAnswer.toLowerCase();
 if (petAnswer === 'yes' || petAnswer === 'y' || petAnswer === 'yeah'){
@@ -62,43 +62,103 @@ if (petAnswer === 'yes' || petAnswer === 'y' || petAnswer === 'yeah'){
 } else {
   alert('Correct :(');
   // console.log('Correct :(');
+  answersCorrect++;
 }
-
+//  question three ===========================
 var gamesAnswer = prompt('Does Matt enjoy playing games?');
 gamesAnswer = gamesAnswer.toLowerCase();
 if (gamesAnswer === 'yes' || gamesAnswer === 'y' || gamesAnswer === 'yeah'){
   alert('Absolutely!');
   // console.log('Absolutely!');
+  answersCorrect++;
 } else {
   alert('What?! Of course I like games!');
   // console.log('What?! Of course I like games!');
 }
-
-var daysHaircutAnswer = prompt('How long (in days) has it been since Matt last had a harciut (usually I get a haircut every 28 days)?');
-daysHaircutAnswer = parseInt(daysHaircutAnswer);
-if (daysHaircutAnswer === 85){
-  alert('Yes! How\'d you guess? It\'s been far too long!');
-  // console.log('Yes! How\'d you guess? It\'s been far too long!');
-} else if (daysHaircutAnswer < 85){
-  alert('I wish! Longer!');
-  // console.log('I wish! Longer!');
+// question four =================================
+var askMovieAnswer = prompt('Does Matt miss going to the movies?').toLowerCase();
+if (askMovieAnswer === 'yes' || askMovieAnswer === 'y' || askMovieAnswer === 'yeah'){
+  alert('Yup, I\'m very bummed that I can\'t go to the movie theater.');
+  answersCorrect++;
 } else {
-  alert('You kidding? My hair would be way longer if it had been that long!');
-  // console.log('You kidding? My hair would be way longer if it had been that long!');
+  alert('Wrong! I never realized I would take movie theaters for granted.');
 }
 
-var seasonPreferenceAnswer = prompt('Does Matt prefer Summer, Winter, Fall, or Spring?');
-seasonPreferenceAnswer = seasonPreferenceAnswer.toLowerCase();
+
+// quesiton five ==============================
+var seasonPreferenceAnswer = prompt('Does Matt prefer Summer, Winter, Fall, or Spring?').toLowerCase();
 if (seasonPreferenceAnswer === 'summer'){
   alert('Yes! But usually by the end of it I\'m excited for Fall. I hope you enjoy the Fall, too, ' + askNameAnswer + '!');
+  answersCorrect++;
   // console.log('Yes! But usually by the end of it I\'m excited for Fall. I hope you enjoy the Fall, too, ' + askNameAnswer + '!');
 } else if (seasonPreferenceAnswer === 'fall'){
   alert('Yes! But usually by the end of it I\'m excited for Winter. I hope you enjoy the Winter, too, ' + askNameAnswer + '!');
+  answersCorrect++;
   // console.log('Yes! But usually by the end of it I\'m excited for Winter. I hope you enjoy the Winter, too, ' + askNameAnswer + '!');
 } else if (seasonPreferenceAnswer === 'winter'){
   alert('Yes! But usually by the end of it I\'m excited for Spring. I hope you enjoy the Spring, too, ' + askNameAnswer + '!');
+  answersCorrect++;
   // console.log('Yes! But usually by the end of it I\'m excited for Spring. I hope you enjoy the Spring, too, ' + askNameAnswer + '!');
 } else {
   alert('Yes! But usually by the end of it I\'m excited for Summer. I hope you enjoy the Summer, too, ' + askNameAnswer + '!');
+  answersCorrect++;
   // console.log('Yes! But usually by the end of it I\'m excited for Summer. I hope you enjoy the Summer, too, ' + askNameAnswer + '!');
 }
+
+// var daysHaircutAnswer = prompt('How long (in days) has it been since Matt last had a harciut (usually I get a haircut every 28 days)?');
+// daysHaircutAnswer = parseInt(daysHaircutAnswer);
+// if (daysHaircutAnswer === 85){
+//   alert('Yes! How\'d you guess? It\'s been far too long!');
+//   // console.log('Yes! How\'d you guess? It\'s been far too long!');
+// } else if (daysHaircutAnswer < 85){
+//   alert('I wish! Longer!');
+//   // console.log('I wish! Longer!');
+// } else {
+//   alert('You kidding? My hair would be way longer if it had been that long!');
+//   // console.log('You kidding? My hair would be way longer if it had been that long!');
+// }
+
+// question six ======================================
+var haircutAttempts = 0;
+var daysHaircut = 85;
+var daysHaircutAnswer;
+while(haircutAttempts < 4 && daysHaircutAnswer !== daysHaircut){
+  daysHaircutAnswer = prompt('How long (in days) has it been since Matt last had a harciut (usually I get a haircut every 28 days)?');
+  daysHaircutAnswer = parseInt(daysHaircutAnswer);
+  if (daysHaircutAnswer < 85){
+    alert('I wish! Longer!');
+  } else if (daysHaircutAnswer > 85){
+    alert('You kidding? My hair would be way longer if it had been that long!');
+  } else if (daysHaircutAnswer === 85){
+    alert('Yes! How\'d you guess? It\'s been far too long!');
+    answersCorrect++;
+  } else {
+    alert('Please enter a number!');
+  }
+  haircutAttempts++;
+  console.log(haircutAttempts);
+
+} if(haircutAttempts === 4 && daysHaircutAnswer !== daysHaircut){
+  alert('Actually, Matt hasn\'t had a haircut in 85 days!');
+}
+
+var favoriteCheeses = ['brie', 'aged cheddar', 'parmesan'];
+var cheeseGuess;
+var cheeseAttempts = 0;
+while(cheeseAttempts < 6 && (cheeseGuess !== favoriteCheeses[0] || cheeseGuess !== favoriteCheeses[1] || cheeseGuess !== favoriteCheeses[2])){
+  cheeseGuess = prompt('What is one of Matt\'s favorite kind of cheese?').toLowerCase();
+  if (cheeseGuess === favoriteCheeses[0]){
+    alert('Yep! I love ' + favoriteCheeses[0] + '!');
+    answersCorrect++;
+  } else if (cheeseGuess === favoriteCheeses[1]){
+    alert ('Yep! I love ' + favoriteCheeses[1] + '!');
+    answersCorrect++;
+  } else if (cheeseGuess === favoriteCheeses[2]){
+    alert ('Yep! I love ' + favoriteCheeses[2] + '!');
+    answersCorrect++;
+  } else{
+    alert ('Nope! Try again!');
+  }
+} alert('My favorite kinds of cheese are ' + favoriteCheeses[0] + ', ' + favoriteCheeses[1] + ', and ' + favoriteCheeses[2] + '!');
+
+
